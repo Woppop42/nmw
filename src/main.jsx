1f425796d9d278569d6677685bb67a3ff2933786
Gabriel <gokba27@gmail.com>
2025-02-11 15:01:40 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-
+console.log('React démarre');
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
